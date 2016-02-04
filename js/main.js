0c2271ce13f3efff2418898e90942f172f36e3d1
Martin Denk <wyverns.spirit@gmail.com>
2016-02-04 10:03:27 +0100

    use logger and thunk

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {createStore, combineReducers} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
+import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 import * as actions from './actions';
 import reducers from './reducers';
 import levels from './levels';
@@ -9,7 +11,16 @@ import App from './components/App.js';
 
 require('../sass/main.sass');
 
-let store = createStore(reducers);
+const middleware = [
+    createLogger({
+        collapsed: true,
+        level: 'info',
+        predicate: (getState, action) => typeof action.type !== 'undefined'
+    }),
+    thunk
+];
+
+const store = applyMiddleware(...middleware)(createStore)(reducers);
 
 store.dispatch(actions.setSteps(levels[0].steps));
 
